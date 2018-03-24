c50810e39a8f323fdd3540d21772f2a10fb16db4
Rails Udemy Course <railsudemycourse@gmail.com>
2018-03-24 10:36:55 -0400

    Add styling to show template

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-    flash[:notice] = "Article was sucessfully created"
+    flash[:success] = "Article was sucessfully created"
     redirect_to article_path(@article)
      else
        render 'new'
@@ -29,7 +29,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Aricle.find(params[:id])
     @article.destroy
-    flash[:notice] = "Aricle was sucessfully deleted "
+    flash[:success] = "Aricle was sucessfully deleted "
     redirect_to article_path
     
   end
@@ -43,7 +43,7 @@ class ArticlesController < ApplicationController
   
     if @article.update(article_params)
 
-    flash[:notice] = "Article was successfully updated"
+    flash[:success] = "Article was successfully updated"
 
     redirect_to article_path(@article)
 
