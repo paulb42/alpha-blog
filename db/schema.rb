a66d577b681f81e7f2903114974ede5f469dc70d
Rails Udemy Course <railsudemycourse@gmail.com>
2018-04-01 05:24:29 -0400

    signup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180329185629) do
+ActiveRecord::Schema.define(version: 20180331170013) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
