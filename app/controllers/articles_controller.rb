c4bbd854e7cb4b6d38bc2df520e145bac040c1ac
Rails Udemy Course <railsudemycourse@gmail.com>
2018-04-02 13:41:06 -0400

    add pagination

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -58,9 +58,10 @@ class ArticlesController < ApplicationController
 end
   
   def index
+    
+    @articles = Article.paginate(page: params[:page], per_page: 5)
 
-   @articles = Article.all
-
+   
 end
     
   
