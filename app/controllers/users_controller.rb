c3594dc53c414bedf3f3168b92771ddd892e117e
Rails Udemy Course <railsudemycourse@gmail.com>
2018-04-02 10:41:37 -0400

    add users listing and update show article view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+    
+  end
+  
   def new
    @user = User.new
     
@@ -13,6 +19,29 @@ class UsersController < ApplicationController
         render 'new'
       end
     end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to articles_path
+      else
+      render 'edit'
+      end
+    end
+ 
+  def show
+    @user = User.find(params[:id])
+    end
+    
+      
+    
+ 
+    
     
     
     private
