a2da8090cfe1bddc317253da38c7eee80f836779
Michael Slainte <eng.michaelmarques@outlook.com>
2018-04-06 23:43:11 +0100

    make it better

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,9 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+# config/application.rb
+    config.assets.initialize_on_precompile = false
+    
 Bundler.require(*Rails.groups)
 
 module AlphaBlog
@@ -19,7 +22,6 @@ module AlphaBlog
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    # config/application.rb
-    config.assets.initialize_on_precompile = false
+    
   end
 end
